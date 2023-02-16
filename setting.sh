1b5b9cee8ec2175365a1aab3305c60ff29e04d49
Heliss <phisicsmusic@gmail.com>
2023-02-17 03:14:36 +0900

    add wildcard

--- setting.sh
+++ setting.sh
@@ -2,14 +2,14 @@
 source venv.sh
 
 apt update
-apt install vim
-apt install vim -y
+apt install vim -Y
 
 pip install wget
 pip install gitpython
 
 cp SDsetting/ui-config.json ui-config.json
 cp SDsetting/config.json config.json
+cp -r SDsetting/wildcards/* extensions/stable-diffusion-webui-wildcards/wildcards
 
 python SDsetting/modeldownloader.py --set
 
