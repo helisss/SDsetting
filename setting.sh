78f4f52d3d8dc3f5a3147c977a2e8b7ed1b48062
Heliss <phisicsmusic@gmail.com>
2023-02-17 14:39:09 +0900

    setting

--- setting.sh
+++ setting.sh
@@ -10,6 +10,7 @@ pip install gitpython
 cp SDsetting/ui-config.json ui-config.json
 cp SDsetting/config.json config.json
 cp -r SDsetting/wildcards/* extensions/stable-diffusion-webui-wildcards/wildcards
+cp SDsetting/styles.csv styles.csv
 
 python SDsetting/modeldownloader.py --set
 
