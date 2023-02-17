b11f46002143dba5fe762227a94e71dea5acb5c6
Heliss <phisicsmusic@gmail.com>
2023-02-17 16:34:10 +0900

    setting order change

--- setting.sh
+++ setting.sh
@@ -7,10 +7,11 @@ apt -y install vim
 pip install wget
 pip install gitpython
 
-python SDsetting/modeldownloader.py --set
-
 cp SDsetting/ui-config.json ui-config.json
 cp SDsetting/config.json config.json
+
+python SDsetting/modeldownloader.py --set
+
 #cp SDsetting/styles.csv styles.csv
 
 cp -r SDsetting/wildcards/* extensions/stable-diffusion-webui-wildcards/wildcards
