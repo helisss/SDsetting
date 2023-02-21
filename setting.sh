6fb067aa5d3a5f114ef1586ca9ac9cc3ed541e7a
helisss <phisicsmusic@gmail.com>
2023-02-22 02:49:01 +0900

    change setting.sh

--- setting.sh
+++ setting.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 #source venv.sh
 
-apt update
-apt -y install vim zip
 
 pip install wget
 pip install gitpython
 
-cp SDsetting/ui-config.json ui-config.json
-cp SDsetting/config.json config.json
-
 python SDsetting/modeldownloader.py --set
-
-#cp SDsetting/styles.csv styles.csv
 
 cp -r SDsetting/wildcards/* extensions/sd-dynamic-prompts/wildcards
 
